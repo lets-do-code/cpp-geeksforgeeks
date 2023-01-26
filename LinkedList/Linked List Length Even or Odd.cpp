db92199534f6696dc6e1b477feb59af1870b0c25
Abhijeet <108912315+lets-do-code@users.noreply.github.com>
2023-01-27 01:04:35 +0530

    Add files via upload

---
+++ LinkedList/Linked List Length Even or Odd.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+// Function to check the length of linklist
+int isLengthEvenOrOdd(struct Node* head);
+
+// Driver function
+int main(void)
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,tmp,first, i ,l;
+        struct Node* head = NULL, *tail = NULL;
+        cin>>n;
+        cin>>first;
+        head = new Node(first);
+        tail = head;
+        for(i=0;i<n-1;i++)
+            {
+                cin>>l;
+                tail->next = new Node(l);
+                tail = tail->next;
+            }
+        if( isLengthEvenOrOdd(head) == 0) cout<<"Even\n";
+        else cout<<"Odd\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/*structure of a node of the linked list is as
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/
+// Function should return 0 is length is even else return 1
+int isLengthEvenOrOdd(struct Node* head)
+{
+    Node*temp=head;
+    
+    int length=0;
+    
+    while(temp!=NULL){
+        temp=temp->next;
+        length++;
+    }
+    if(length%2!=0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
